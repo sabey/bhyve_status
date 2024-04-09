e319452e7772096749457c61d55494f3c0b99e51
Jackson Sabey <jackson@juandefu.ca>
2024-04-08 20:46:30 -0700

    ignore locked

--- main.go
+++ main.go
@@ -34,6 +34,8 @@ const (
 	vm_list = `NAME    DATASTORE  LOADER  CPU  MEMORY  VNC  AUTO      %CPU       RSZ          UPTIME  STATE
 	ubuntu  default    grub    4    16G     -    Yes [1]    0.0    463.0M           07:27  Running (28159)
 	windows default    grub    4    16G     -    Yes [1]      -         -               -  Stopped
+	freebsd default    grub    4    16G     -    Yes [1]      -         -               -  Locked (router.pfsense.arpa)
+	pfsense default    grub    4    16G     -    Yes [1]  Locked (router.pfsense.arpa)
 `
 )
 
@@ -110,8 +112,9 @@ func ScanVMS(
 		name := row[0]
 		state := row[len(row)-1]
 		running := !(state == "Stopped")
+		locked := (row[len(row)-2] == "Locked")
 		pid := 0
-		if running {
+		if !locked && running {
 			if !strings.HasPrefix(state, "(") ||
 				!strings.HasSuffix(state, ")") {
 				log.Fatalf("running vm %q does not have a pid, got %q\n", name, line)
--- vm_list.sh
+++ vm_list.sh
@@ -2,4 +2,6 @@
 echo "NAME    DATASTORE  LOADER  CPU  MEMORY  VNC  AUTO      %CPU       RSZ          UPTIME  STATE
 	ubuntu  default    grub    4    16G     -    Yes [1]    0.0    463.0M           07:27  Running (28159)
 	windows default    grub    4    16G     -    Yes [1]      -         -               -  Stopped
+	freebsd default    grub    4    16G     -    Yes [1]      -         -               -  Locked (router.pfsense.arpa)
+	pfsense default    grub    4    16G     -    Yes [1]  Locked (router.pfsense.arpa)
 "
